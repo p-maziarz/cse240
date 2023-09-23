7c6812b0d0bc5430d72ed06724f6ff96e2feb2a7
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-09-23 14:35:46 -0700

    Update guessingGameUpdated.c

--- guessingGameUpdated.c
+++ guessingGameUpdated.c
@@ -5,11 +5,20 @@
 //fixed code so it was fully functional as it wasn't 100% functional last time i submitted
 
 /* TODO:
-- keep the name and number of guesses in a data structure like a struct
 - keep an array of 5 structs to store the current leaders
 - when the user quits, the current leaders should be saved to a text file
 - when the game is started, the file should be read, if it exists
  */
+
+struct playerInfo {
+    char name[100];
+    int guessCount;
+};
+
+struct scoreBoard {
+    char scores[5];
+    int highScore;
+};
 
 int getGuess() {
     int guess;
@@ -31,7 +40,7 @@ void playGuessingGame(struct playerInfo *playerInfo) { //
     while (1) {
         guess = getGuess();
         guessCount++; //count increases by 1 each guess
-        playerInfo->guessCount++; //adds incremented guess count
+        playerInfo->guessCount++; //adds incremented guess count to playerInfo
 
         if (guess < num) {
             printf("Too low, guess again: ");
@@ -47,10 +56,7 @@ void playGuessingGame(struct playerInfo *playerInfo) { //
 
 int main() {
     char c;
-    struct playerInfo player { //struct to store players info
-        char name[100];
-        int guessCount;
-    };
+    struct playerInfo player;
     char name[100]; //char array to store name as string, name can be up to 100 characters
     srand(time(NULL)); //random number generator with current time
     printf("Welcome! Please enter your name: "); //prompts user for name
