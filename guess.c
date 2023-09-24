7dea584db8b678a18f2b06bf63d635587395adab
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-09-24 13:26:05 -0700

    Update and rename guessingGameUpdated.c to guess.c
    Renamed file to meet assignment criteria
    Added extra comments to specify each method 
    Made sure text file was saved/loaded as relative filename instead of local path for grader convenience

--- guess.c
+++ guess.c
@@ -4,15 +4,18 @@
 #include <time.h>
 #include <string.h>
 
+//this struct stores the player's name as an array of 100 chars and an int for the amount of guesses a user has tried
 struct playerInfo {
     char name[100];
     int guessCount;
 };
 
-struct leaderboard { //defines leaderboard as a struct to store the leaderboard
+//this defines the leaderboard as a struct to store the data of the top 5 players
+struct leaderboard {
     struct playerInfo players[5];
 };
 
+//this function collects the user's guess and stores it as an int
 int getGuess() {
     int guess;
     printf("Enter your guess: ");
@@ -20,14 +23,15 @@ int getGuess() {
     return guess;
 }
 
-void playGuessingGame(struct playerInfo *playerInfo) { //
+//this function provides the code for the actual game, like generating the random number, and checking if answer is correct or not
+void playGuessingGame(struct playerInfo *playerInfo) {
     int num;
     double sqrtValue;
     int guess;
-    int guessCount = 0; //declares guess count as 0 to start
+    int guessCount = 0; //declares starting guess count as 0
 
-    num = (rand() % 91) + 10;
-    sqrtValue = sqrt(num);
+    num = (rand() % 91) + 10; //generates a random number and restricts it to only generate from 10-100
+    sqrtValue = sqrt(num); //typecasts the square root of the number as a double bc many square roots of integers are not also integers
     printf("%.15lf is the square root of what number? \n", sqrtValue);
 
     while (1) {
@@ -35,33 +39,36 @@ void playGuessingGame(struct playerInfo *playerInfo) { //
         guessCount++; //count increases by 1 each guess
         playerInfo->guessCount++; //adds incremented guess count to playerInfo
 
-        if (guess < num) {
+        if (guess < num) { //runs the answer through these if-else statements to check if whether the answer is correct or not
             printf("Too low, guess again: \n");
         } else if (guess > num) {
             printf("Too high, guess again: \n");
         } else {
             printf("You got it, baby!\n");
             printf("Congratulations! You guessed the number in %d tries.\n", guessCount);
-            break;
+            break; //ends that if-else statement when the correct answer is input
+                    // and displays the amount of times the player tried to guess
         }
     }
 }
 
-void saveLeaderboard(struct leaderboard *leaderboard) { //saves leaderboard to text file
-    FILE *file = fopen("leaderboard.txt", "w");
+//this function saves leaderboard to text file called leaderboard.txt
+void saveLeaderboard(struct leaderboard *leaderboard) {
+    FILE *file = fopen("leaderboard.txt", "w"); //only initializes as a filename instead of a full path
     if (file == NULL) {
-        printf("Error opening file for writing.\n");
+        printf("Error opening file for writing.\n"); //checks if the file exists or not
         return;
     }
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) { //writes leaderboard data to the file
         fprintf(file, "%s %d\n", leaderboard->players[i].name, leaderboard->players[i].guessCount);
     }
 
-    fclose(file);
+    fclose(file); //closes file
 }
 
-void loadLeaderboard(struct leaderboard *leaderboard) { //loads the leaderboard from a text file
+//this function loads the leaderboard from that text file
+void loadLeaderboard(struct leaderboard *leaderboard) {
     FILE *file = fopen("leaderboard.txt", "r");
     if (file == NULL) {
         for (int i = 0; i < 5; i++) {
@@ -73,7 +80,7 @@ void loadLeaderboard(struct leaderboard *leaderboard) { //loads the leaderboard 
 
     for (int i = 0; i < 5; i++) { //reads leaderboard data from file
         if (fscanf(file, "%99s %d", leaderboard->players[i].name, &leaderboard->players[i].guessCount) != 2) {
-            for (int j = i; j < 5; j++) { //error reading data, initalize leaderboard
+            for (int j = i; j < 5; j++) { //initializes leaderboard if there's an error reading the data
                 strcpy(leaderboard->players[j].name, "");
                 leaderboard->players[j].guessCount = 0;
             }
@@ -81,9 +88,11 @@ void loadLeaderboard(struct leaderboard *leaderboard) { //loads the leaderboard 
         }
     }
 
-    fclose(file);
+    fclose(file); //closes file
 }
 
+//this main method is the method that builds the complete game off of the functions I made above
+//it also runs the code to store the user's name and if they want to continue playing or quit
 int main() {
     char c;
     struct playerInfo player;
@@ -93,7 +102,7 @@ int main() {
     printf("Welcome! Please enter your name: \n"); //prompts user for name
     scanf("%s", player.name);
     printf("Thank you, %s! Press 'q' to quit or any key to continue:\n", name);
-    scanf(" %c", &c);
+    scanf(" %c", &c); //checks the user's key input to determine if the user wants to play or quit
 
     struct playerInfo leaderBoard[5]; //initializes leaderboard array
     for (int i = 0; i < 5; i++){ //sets guess count to 0 for all players
@@ -104,6 +113,7 @@ int main() {
     struct leaderboard currentLeaderboard; //creates leaderboard and loads data from text file if it exists
     loadLeaderboard(&currentLeaderboard);
 
+    //if the player presses any key besides q, they want to play, so this code runs
     while (c != 'q') {
         playGuessingGame(&player);
 
