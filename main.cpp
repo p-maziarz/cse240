9a0726283700b53eb88964a5ee9cd1f1cb209a00
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-28 13:56:32 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include "pet.hpp"
+#include "dog.hpp"
+#include "cat.hpp"
+
+using namespace std;
+
+//Calculate license fees
+//Rate can be set to $2/lb for a dog, $1.50/lb for a cat, output the name of the pet and the fee
+int main() {
+
+    return 0;
+}
