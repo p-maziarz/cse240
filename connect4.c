9b02a603cf89859e48d7c4d10860eff122321ee0
pmaziarz <parkermaziarz@gmail.com>
2024-07-22 17:15:51 -0700

    sss

--- connect4.c
+++ connect4.c
@@ -34,7 +34,7 @@ int GetEmptyRow(char board[][NUM_COLS], int col) {
     }
     return -1; // Returns -1 if there are no empty rows available
 }
-
+//dsdsdsdsds
 bool CheckBoardFull(char board[][NUM_COLS]) {
     for (int row = 0; row < NUM_ROWS; row++) {
         for (int col = 0; col < NUM_COLS; col++) {
